188d192570896cc79763b9bb57e703093ab5fe77
omc10079 <kida-seiji@orient-microwave.co.jp>
2024-04-14 17:22:28 +0900

    v1.2.0

--- PCAB_Debugger_RP2040/PCAB_Debugger_FW/PCAB_Debugger_FW.hpp
+++ PCAB_Debugger_RP2040/PCAB_Debugger_FW/PCAB_Debugger_FW.hpp
@@ -14,29 +14,29 @@
 #define SPI0_RX_PIN 4
 #define SPI0_LE_PIN 5
 #define SPI0_CLK_PIN 6
-#define SPI1_TX_PIN 7
-#define SPI1_RX_PIN 4
-#define SPI1_LE_PIN 5
-#define SPI1_CLK_PIN 6
+#define SPI1_TX_PIN 15
+#define SPI1_RX_PIN 12
+#define SPI1_LE_PIN 13
+#define SPI1_CLK_PIN 14
 // Onewire PIN Configure
-#define SNS_TEMP_PIN 24
+#define SNS_TEMP_PIN 25
 // MODE PIN Configure
 #define LPW_MOD_PIN 18
 #define STB_DRA_PIN 19
 #define STB_AMP_PIN 20
 #define STB_LNA_PIN 21
-#define DSA_D0_PIN 21
-#define DSA_D1_PIN 21
-#define DSA_D2_PIN 21
-#define DSA_D3_PIN 21
-#define DSA_D4_PIN 21
+#define DSA_D0_PIN 16
+#define DSA_D1_PIN 17
+#define DSA_D2_PIN 22
+#define DSA_D3_PIN 23
+#define DSA_D4_PIN 24
 // Switch PIN Configure
-#define SW_1_PIN 8
-#define SW_2_PIN 9
-#define SW_3_PIN 10
-#define SW_4_PIN 11
-#define SW_5_PIN 12
-#define SW_6_PIN 13
+#define SW_1_PIN 2
+#define SW_2_PIN 3
+#define SW_3_PIN 8
+#define SW_4_PIN 9
+#define SW_5_PIN 10
+#define SW_6_PIN 11
 
 // UART Configure
 //#define UART_BAUD_RATE 9600
